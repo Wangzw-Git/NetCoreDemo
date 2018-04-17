92cfdfd02105cf680a20af19ee613e64161623cc
Wangzw-Git <892017437@qq.com>
2018-04-17 09:37:16 +0800

    Initial commit

---
+++ Mvc01/AppSettingsModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mvc01
+{
+    /// <summary>
+    /// appsettings映射实体类
+    /// </summary>
+    public class AppSettingsModel
+    {
+        public int BranchId { get; set; }
+
+        public string BranchName { get; set; }
+
+        public ICollection<UserAccount> UserAccounts { get; set; }
+    }
+
+    public class UserAccount
+    {
+        public int UserId { get; set; }
+
+        public string UserName { get; set; }
+    }
+}
---
+++ Mvc01/Startup.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Mvc01
+{
+    public class Startup
+    {
+        private IConfiguration Configuration { get; set; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                //bind方法读取appsettings.json
+                AppSettingsModel appSettings = new AppSettingsModel();
+                Configuration.Bind(appSettings);
+
+                await context.Response.WriteAsync($"BarnchId:{appSettings.BranchId}\r\n");
+                await context.Response.WriteAsync($"BarnchName:{appSettings.BranchName}\r\n");
+                foreach (var item in appSettings.UserAccounts)
+                {
+                    await context.Response.WriteAsync($"UserId:{item.UserId}，UserName:{item.UserName}\r\n");
+                }
+            });
+        }
+    }
+}
